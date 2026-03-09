ccfed97e90811b90c05af845f18c55bbec3b264e
UnbreakablePotato <sanderlange@gmail.com>
2026-03-09 13:10:25 +0100

    Updated the game loop to support rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
 from logger import log_state
+import player
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -8,14 +9,19 @@ dt = 0
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
 def gameloop():
+    p1 = player.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2) 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        p1.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60)/1000
+        p1.update(dt)
+        
         
 
 
