14b2d78316f01469536dc106a69ea78f569e5af7
UnbreakablePotato <sanderlange@gmail.com>
2026-03-09 12:42:51 +0100

    Added circle class to creater both player and asteroids

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import constants
 from logger import log_state
 
 pygame.init()
-
+clock = pygame.time.Clock()
+dt = 0
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
 def gameloop():
@@ -14,6 +15,8 @@ def gameloop():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
