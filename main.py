bf7059e4f491b17f36506e041f41636cc01373d7
UnbreakablePotato <sanderlange@gmail.com>
2026-03-09 23:29:45 +0100

    Added the collision detection logic to the game loop

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 import constants
 from logger import log_state
+from logger import log_event
 import player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 pygame.init()
 clock = pygame.time.Clock()
-dt = 0
+
 
 
 
@@ -13,22 +17,35 @@ def gameloop():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     player.Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    ast = AsteroidField()
     p1 = player.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2) 
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
         screen.fill("black")
 
         for p in drawable:
             p.draw(screen)
+            for asteroid in asteroids:
+                if asteroid.collides_with(p1):
+                    log_event("player_hit")
+                    print("Game over!")
+                    sys.exit()
         
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        for p in updatable:
-            p.update(dt)
+
+
         
         
 
